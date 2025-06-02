59cd58992c2e11f9a5926605623c6d793f77fc6c
kizsbrownOG1 <sponkibrownO.G@gmail.com>
2025-06-02 00:51:42 -0400

    good

--- Resources/stylesheet/multilanguage.js
+++ Resources/stylesheet/multilanguage.js
@@ -1,6 +1,8 @@
 
  const translations = {
             en: {
+                topic: "FULLSTACK DEVELOPER",
+
                 porfolio: "PORFOLIO",
                 aboutme: "ABOUT-ME",
                 project: "PROJECTS",
@@ -11,11 +13,19 @@
                 projects: "PROJECTS",
                 services: "SERVICES",
 
-                greeting: "FULLSTACK DEVELOPER"
+                s: "Hello, I`m  Okafor Goodluck Chibuike",
+                h: "Software developer with a passion of creating",
+                o: "interactive and user-friendly Website/Application,",
+                r: "to bring your design to life and making them functional",
+                t: "and make solutions to your problems.",
+
+                abtme: "About Me"
                 
             },
 
             fr: {
+                topic: "DEVELOPPEUR FULLSTACK",
+
                 porfolio: "PORFOLIO",
                 aboutme: "A PROPOS DE MOI",
                 project: "PROJETS",
@@ -26,8 +36,15 @@
                 projects: "PROJETS",
                 services: "SERVICES",
 
-                greeting: "DEVELOPPEUR FULLSTACK"
-            },
+                s: "Salut, je suis Okafor Chibuike Goodluck",
+                h: "Developpeur logiciel passionne par creation",
+                o: "Site Web/Application interactif(ve) et facile a utiliser,",
+                r: "Donner vie a vos designs et les rendre fonctionnels",
+                t: "Et creer des solutions a vos problemes",
+
+                abtme: "A propos de me"
+                
+            }
         };
 
 
@@ -38,15 +55,27 @@
         function setLanguage(lang){
             currentLanguage = lang;
 
+            document.getElementById("h2").textContent = translations[lang].topic;
+
             document.getElementById("porfolio").textContent = translations[lang].porfolio;
             document.getElementById("about-me").textContent = translations[lang].aboutme;
             document.getElementById("project").textContent = translations[lang].project;
             document.getElementById("service").textContent = translations[lang].service;
+
             document.getElementById("porfolios").textContent = translations[lang].porfolios;
             document.getElementById("about-mes").textContent = translations[lang].aboutmes;
             document.getElementById("projects").textContent = translations[lang].projects;
             document.getElementById("services").textContent = translations[lang].services;
-            // document.getElementById("farewell").textContent = translations[lang].farewell;
-            document.getElementById("h2").textContent = translations[lang].greeting;
+
+            document.getElementById("s").textContent = translations[lang].s;
+            document.getElementById("h").textContent = translations[lang].h;
+            document.getElementById("o").textContent = translations[lang].o;
+            document.getElementById("r").textContent = translations[lang].r;
+            document.getElementById("t").textContent = translations[lang].t;
+
+            document.getElementById("abtme").textContent = translations[lang].abtme;
+            
+            
+            
             
         }
